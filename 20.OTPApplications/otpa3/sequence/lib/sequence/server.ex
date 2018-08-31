f3801929d9ff2f38615a21438e5177167c82a527
Ken Chen <chengusky@gmail.com>
2018-08-31 08:00:46 +0800

    Prepare 20.OTPApplications 3 Done

--- 20.OTPApplications/otpa3/sequence/lib/sequence/server.ex
+++ 20.OTPApplications/otpa3/sequence/lib/sequence/server.ex
@@ -1,37 +1,52 @@
 defmodule Sequence.Server do
   use GenServer
 
-  @vsn "0"
+  @vsn "1"
+
+  defmodule State do
+    defstruct(current_number: 0, delta: 1)
+  end
 
   # External API
   def start_link(init_number) do
     GenServer.start_link(__MODULE__, init_number, name: __MODULE__)
   end
 
-  def update(val) do
-    GenServer.cast(__MODULE__, { :update, val })
+  def increment_number(delta) do
+    GenServer.cast(__MODULE__, { :increment_number, delta })
   end
 
   def next_number() do
-    with number = GenServer.call(__MODULE__, :get),
+    with number = GenServer.call(__MODULE__, :next_number),
     do: "The next number is #{number}"
   end
 
   # GenServer Implementation
   def init(_) do
-    { :ok, Sequence.Stash.get() }
+    state = %State{ current_number: Sequence.Stash.get() }
+    { :ok, state }
   end
 
-  def handle_cast({ :update, val }, _current_number) do
-    { :noreply, val }
+  def handle_cast({ :increment_number, delta }, state) do
+    { :noreply, %{ state | delta: delta } }
   end
 
-  def handle_call(:get, _from, current_number) do
-    { :reply, current_number, current_number + 1 }
+  def handle_call(:next_number, _from, state = %{ current_number: n }) do
+    { :reply, n, %{ state | current_number: n + state.delta } }
   end
 
   def terminate(_reason, state) do
     IO.puts "State to be persisted: #{inspect state}"
     Sequence.Stash.update(state)
+  end
+
+  def code_change("0", old_state = current_number, _extra) do
+    new_state = %State{
+      current_number: current_number,
+      delta: 1
+    }
+    IO.puts inspect old_state
+    IO.puts inspect new_state
+    { :ok, new_state }
   end
 end
